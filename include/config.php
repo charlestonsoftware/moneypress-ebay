43146931e1614963a6b637f35c420ced0b7d676f
Eric James Michael Ritz <Eric@cybersprocket.com>
2010-05-10 15:01:45 -0400

    Die when missing required sub-modules
    The current behavior is the require the sub-modules if they are
    present, but if they are not present we cannot continue running the
    plugin, so we now come to an abrupt halt.

--- include/config.php
+++ include/config.php
@@ -1,11 +1,25 @@
 <?php
 
+/**
+ * We need both of the WPCSL-generic and WPCSL-license sub-modules to
+ * be available for the plugin to work.  Otherwise we die immediately
+ * with a cryptic message to the user, which means we haven't done our
+ * job very well---we should never be releasing the plugin without
+ * these sub-modules.
+ */
+
 if (class_exists('wpCSL_Settings') === false) {
     require_once(CJPLUGINDIR.'/WPCSL-generic/CSL-generic.php');
+}
+else {
+    die('Missing WPCSL-generic sub-module.');
 }
 
 if (function_exists('wpCSL_check_license_key') === false) {
     require_once(CJPLUGINDIR.'/WPCSL-license/CSL-license.php');
+}
+else {
+    die('Missing WPCSL-license sub-module.');
 }
 
 ?>
