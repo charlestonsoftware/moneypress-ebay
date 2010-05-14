b287bf16424e4586802d0a39848ea5de640f5511
Eric James Michael Ritz <Eric@cybersprocket.com>
2010-05-14 13:56:55 -0400

    Update the eBay driver to have better error handling
    Previously the eBay driver would die if it were missing any
    requirements, but now it throws an exception in that case.  Instead of
    causing Wordpress to die, we now catch that exception and post a
    notification on the admin page informing the the user that he is
    missing a required library.
    
    Redmine-Ticket: 2112

--- mp-ebay.php
+++ mp-ebay.php
@@ -34,8 +34,14 @@ define('CJPLUGINURL', plugins_url('',__FILE__));
 require_once('include/config.php');
 
 if (class_exists('eBayPanhandler') === false) {
-    require_once('Panhandler/Panhandler.php');
-    require_once('Panhandler/Drivers/eBay.php');
+    try {
+        require_once('Panhandler/Panhandler.php');
+        require_once('Panhandler/Drivers/eBay.php');
+    }
+    catch (PanhandlerMissingRequirement $exception) {
+        add_action('admin_notices', array($exception, 'getMessage'));
+        exit(1);
+    }
 }
 
 add_filter('wp_print_styles', 'MP_ebay_user_css');
