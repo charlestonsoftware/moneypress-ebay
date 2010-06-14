19683f257473a545683947a2617641dce54abd46
Eric James Michael Ritz <Eric@cybersprocket.com>
2010-06-14 09:40:27 -0400

    Test for the existence of Panhandler interface before loading
    The plugin only checks to see whether or not eBayPanhandler exists
    before loading the interface and the driver.  If the user has multiple
    MoneyPress plugins installed then this will cause a conflict with a
    pre-loaded version of the Panhandler interface.
    
    Redmine-Ticket: 2256

--- mp-ebay.php
+++ mp-ebay.php
@@ -38,9 +38,18 @@ if (defined('MP_EBAY_PLUGINURL') === false) {
 
 require_once('include/config.php');
 
-if (class_exists('eBayPanhandler') === false) {
+if (class_exists('PanhandlerProduct') === false) {
     try {
         require_once('Panhandler/Panhandler.php');
+    }
+    catch (PanhandlerMissingRequirement $exception) {
+        add_action('admin_notices', array($exception, 'getMessage'));
+        exit(1);
+    }
+}
+
+if (class_exists('eBayPanhandler') === false) {
+    try {
         require_once('Panhandler/Drivers/eBay.php');
     }
     catch (PanhandlerMissingRequirement $exception) {
