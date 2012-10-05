5692047f93979750c3f36b55dda082712f5af885
Lance Cleveland <lance@cybersprocket.com>
2012-10-04 21:47:51 -0400

    Fix general settings button.

--- templates/navbar.php
+++ templates/navbar.php
@@ -7,5 +7,5 @@
 ?>
 
 <ul>
-    <li class='like-a-button'><a href="/wp-admin/options-general.php?page=csl-mp-ebay-options">Settings: General</a></li>       
+    <li class='like-a-button'><a href="<?php echo admin_url(); ?>options-general.php?page=csl-mp-ebay-options">Settings: General</a></li>
 </ul>
